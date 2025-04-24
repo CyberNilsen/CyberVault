7e27d3455b34e8b25429c5fc0cfe51111cd05b23
Barnegraver <andreasnilsen07@hotmail.com>
2025-04-24 19:50:33 +0200

    Moved aes encryption to main instead of viewmodel

--- CyberVault/Main/PasswordStorage.cs
+++ CyberVault/Main/PasswordStorage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using CyberVault;
+using CyberVault.Main;
 
 namespace CyberVault
 {
--- CyberVault/Viewmodel/AuthenticatorControl.xaml.cs
+++ CyberVault/Viewmodel/AuthenticatorControl.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using CyberVault.Main;
 using CyberVault.View;
 
 namespace CyberVault.Viewmodel
