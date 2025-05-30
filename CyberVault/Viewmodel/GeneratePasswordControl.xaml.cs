691ab8a6248f90a18320b2ee5567df7fdad4cd29
Andreas Nilsen <andreasnilsen07@hotmail.com>
2025-05-30 10:23:31 +0200

    Frond end for generate password is added.

---
+++ CyberVault/Viewmodel/GeneratePasswordControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace CyberVault.Viewmodel
+{
+
+    public partial class GeneratePasswordControl : UserControl
+    {
+        private string _username;
+        private byte[] _encryptionKey;
+        public GeneratePasswordControl(string username, byte[] encryptionKey)
+        {
+            InitializeComponent();
+            _username = username;
+            _encryptionKey = encryptionKey;
+        }
+
+        private void CopyPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GeneratePassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SavePassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
